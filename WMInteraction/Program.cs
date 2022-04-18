06839c9925eb749ae61dd58e9d7e4328dab2c870
NAVNAV221 <navve2008@gmail.com>
2022-04-18 16:13:06 +0300

    Add Query class for WQL quries

---
+++ WMInteraction/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Management;
+
+namespace WMInteraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WMI_component operatingSystemAPI = new WMI_component("ROOT\\cimv2", "Win32_OperatingSystem", "*");
+            operatingSystemAPI.RetriveWMIData();
+        }
+    }
+}
