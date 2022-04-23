f4c86d0bf96e70fcd949492d6f332c480397c2b2
NAVNAV221 <navve2008@gmail.com>
2022-04-23 10:45:19 +0300

    Add association table for many-to-many connection between Client and WMIAction

--- app/models.py
+++ app/models.py
@@ -2,19 +2,24 @@ from app import db
 
 CLIENT_PROPERTIES = ['id', 'os_name', 'ip_address', 'wmi_actions']
 
+client_wmiAction = db.Table('client_wmiAction',
+                            db.Column('client_id', db.Integer, db.ForeignKey('client.id')),
+                            db.Column('wmi_action_id', db.Integer, db.ForeignKey('wmi_action.id')))
+
 
 class Client(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    os_name = db.Column(db.String(64), index=True, unique=True)
+    os_name = db.Column(db.String(64), index=True)
     ip_address = db.Column(db.String(64), index=True, unique=True)
-    wmi_actions = db.relationship('WMIAction', backref='client', lazy='dynamic')
+    wmi_action = db.relationship('WMIAction', secondary=client_wmiAction,
+                                 primaryjoin=(client_wmiAction.c.client_id == id),
+                                 backref='client_wmi_action')
 
     def to_dict(self):
         client_data = {
             'id': self.id,
             'os_name': self.os_name,
             'ip_address': self.ip_address,
-            'wmi_actions': self.wmi_actions
         }
         return client_data
 
@@ -24,12 +29,11 @@ class Client(db.Model):
                 setattr(self, field, client_data[field])
 
     def __repr__(self):
-        return f'[User Description] {self.id} | {self.os_name} | {self.ip_address} | {self.wmi_actions}'
+        return f'[User Description] {self.id} | {self.os_name} | {self.ip_address}'
 
 
 class WMIAction(db.Model):
-    action_id = db.Column(db.String(64), primary_key=True)
+    id = db.Column(db.String(64), primary_key=True)
     wmi_class = db.Column(db.String(64), index=True, unique=True)
-    wmi_attributes = db.Column(db.String(128), index=True, unique=True)
-    wmi_scope = db.Column(db.String(64), index=True, unique=True)
-    client_id = db.Column(db.Integer, db.ForeignKey('client.id'))
+    wmi_attributes = db.Column(db.String(128), index=True)
+    wmi_scope = db.Column(db.String(64), index=True)
