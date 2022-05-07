ab0f2a523f512fe38548f2960672295c74d0a2ae
NAVNAV221 <navve2008@gmail.com>
2022-05-07 16:22:37 +0300

    Use blueprint for the api functionality

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,14 @@
 from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.run(port=8090)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-from app import routes
+from app import routes, models
+from app.api import api_blueprint
+
+app.register_blueprint(api_blueprint, url_prefix='/api')
