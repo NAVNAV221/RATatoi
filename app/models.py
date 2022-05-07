d71b7be10e1af232edd52ee56c1021671dc5abb4
NAVNAV221 <navve2008@gmail.com>
2022-05-07 16:19:41 +0300

    Create access to the client WMI actions

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from app import db
 
 CLIENT_PROPERTIES = ['id', 'os_name', 'ip_address', 'wmi_actions']
+WMI_PROPERTIES = ['id', 'wmi_class', 'wmi_attributes', 'wmi_scope']
 
 client_wmiAction = db.Table('client_wmiAction',
                             db.Column('client_id', db.Integer, db.ForeignKey('client.id')),
@@ -14,6 +15,10 @@ class Client(db.Model):
     wmi_action = db.relationship('WMIAction', secondary=client_wmiAction,
                                  primaryjoin=(client_wmiAction.c.client_id == id),
                                  backref='client_wmi_action')
+
+    @property
+    def wmi_action_dict(self):
+        return [wmi_action.to_dict() for wmi_action in self.wmi_action]
 
     def to_dict(self):
         client_data = {
@@ -29,7 +34,7 @@ class Client(db.Model):
                 setattr(self, field, client_data[field])
 
     def __repr__(self):
-        return f'[User Description] {self.id} | {self.os_name} | {self.ip_address}'
+        return f'[User Description] {self.id} | {self.os_name} | {self.ip_address} | {self.wmi_action}'
 
 
 class WMIAction(db.Model):
@@ -37,3 +42,17 @@ class WMIAction(db.Model):
     wmi_class = db.Column(db.String(64), index=True, unique=True)
     wmi_attributes = db.Column(db.String(128), index=True)
     wmi_scope = db.Column(db.String(64), index=True)
+
+    def to_dict(self):
+        wmi_data = {
+            'id': self.id,
+            'wmi_class': self.wmi_class,
+            'wmi_attributes': self.wmi_attributes,
+            'wmi_scope': self.wmi_scope
+        }
+        return wmi_data
+
+    def from_dict(self, wmi_data):
+        for field in WMI_PROPERTIES:
+            if field in wmi_data:
+                setattr(self, field, wmi_data[field])
