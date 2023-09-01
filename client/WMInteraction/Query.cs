edc1638d59c6be73715ae74064cffa1864e1c85b
NAVNAV221 <navve2008@gmail.com>
2023-09-01 09:07:13 +0300

    Change client directory

---
+++ client/WMInteraction/Query.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Management;
+
+namespace WMInteraction
+{
+    public class Query
+    {
+        private string userScope;
+        private string wmiAttributes;
+        private string wmiClass;
+
+        public Query(string userScope, string wmiAttributes, string wmiClass)
+        {
+            this.userScope = userScope;
+            this.wmiAttributes = wmiAttributes;
+            this.wmiClass = wmiClass;
+        }
+
+        public string WmiAttributes { get; set; }
+
+        public string UserScope { get; set; }
+
+        public string WmiClass { get; set; }
+
+        ObjectQuery BuildQuery(string wmi_attributes, string wmi_class)
+        {
+            ObjectQuery query = new ObjectQuery("SELECT " + wmi_attributes + " FROM " + wmi_class);
+            return query;
+        }
+
+        public ManagementObjectCollection ExecuteQuery()
+        {
+            ObjectQuery query = BuildQuery(this.wmiAttributes, this.wmiClass);
+
+            ManagementScope scope = new ManagementScope(this.userScope);
+            scope.Connect();
+
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(scope, query);
+
+            return searcher.Get();
+        }
+    }
+}
